7306f5a8783ead0233f2429b391b286bffce55ed
Felipe Grando <felipegrando07@gmail.com>
2026-03-23 21:41:17 -0300

    Seguimento do código do professor

--- datamaster_vendas.py
+++ datamaster_vendas.py
@@ -7,6 +7,10 @@ if mes%2 == 0:
 else:    
     dias = 31
 
+total = 0
+total_comissao = 0
+total_com_comissao = 0
+maior_venda = 0
 for vendas in range(dias):
     valor = float(input("Digite o valor da venda: "))
     if valor <= 1000:
@@ -17,3 +21,15 @@ for vendas in range(dias):
         comissao = valor * 0.20
     else:
         comissao = valor * 0.25
+    total += valor
+    total_comissao += comissao
+    total_com_comissao = total + total_comissao
+
+    if valor > maior_venda:
+        maior_venda = valor
+        dia_venda = vendas + 1
+    
+print(f"Total de vendas: R${total:.2f}")
+print(f"Total de comissão: R${total_comissao:.2f}")
+print(f"Lucro: R${total-total_comissao:.2f}")
+print(f"Maior venda: R${maior_venda:.2f} no dia {dia_venda}")
